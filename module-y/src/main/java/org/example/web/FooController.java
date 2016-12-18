b61eba16df7ad07aa06ae867c4d8a53f93c6bec7
Tango Bee <tangobee12@gmail.com>
2016-12-18 04:05:29 +0400

    Fix web.xml

--- module-y/src/main/java/org/example/web/FooController.java
+++ module-y/src/main/java/org/example/web/FooController.java
@@ -2,11 +2,13 @@ package org.example.web;
 
 import org.example.service.FooService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ComponentScan(basePackages = {"org.example.service", "org.example.service"})
 public class FooController
 {
   @Autowired
@@ -16,6 +18,7 @@ public class FooController
   @ResponseBody
   public String home()
   {
+	  
     return service.greet();
   }
 }
